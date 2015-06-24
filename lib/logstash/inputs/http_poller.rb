138656d550704fe681c01ec6a5a63609e810513a
Andrew Cholakian <andrew@andrewvc.com>
2015-06-24 15:38:19 -0500

    Initial sketch of Manticore support

--- lib/logstash/inputs/http_poller.rb
+++ lib/logstash/inputs/http_poller.rb
@@ -3,7 +3,7 @@ require "logstash/inputs/base"
 require "logstash/namespace"
 require "socket" # for Socket.gethostname
 require "json"
-require "rest-client"
+require "manticore"
 
 
 # Read from a list of url returning some json
@@ -33,14 +33,39 @@ class LogStash::Inputs::HTTPPoller < LogStash::Inputs::Base
   # How often  (in seconds) the urls will be called
   config :interval, :validate => :number, :required => true
 
-  # Timeout (in seconds) for the rest call
-  config :timeout, :validate => :number, :default => 60
+  # Timeout (in seconds) for the entire request
+  config :request_timeout, :validate => :number, :default => 60
+
+  # Timeout (in seconds) to wait for data on the socket. Default is 10s
+  config :socket_timeout, :validate => :number, :default => 10
+
+  # Timeout (in seconds) to wait for a connection to be established. Default is 10s
+  config :connect_timeout, :validate => :number, :default => 10
+
+  # Should redirects be followed? Defaults to true
+  config :follow_redirects, :validate => :boolean, :default => true
+
+  # Max number of concurrent connections. Defaults to 50
+  config :pool_max, :validate => :number, :default => 50
+
+  # Max number of concurrent connections to a single host. Defaults to 25
+  config :pool_max_per_route, :validate => :number, :default => 25
+
+  # How many times should the client retry a failing URL? Default is 3
+  config :automatic_retries, :validate => :number, :default => 3
+
+  # Path to trust store (.jks) containing CA certs
+  config :trust_store_path, :validate => :string
+
+  # Password to the trust store if required
+  config :trust_store_path, :validate => :string
+
 
   public
   def register
     @host = Socket.gethostname.force_encoding(Encoding::UTF_8)
 
-    @logger.info("Registering rest Input", :type => @type,
+    @logger.info("Registering http-poller Input", :type => @type,
                  :urls => @urls, :interval => @interval, :timeout => @timeout)
   end # def register
 
@@ -48,31 +73,69 @@ class LogStash::Inputs::HTTPPoller < LogStash::Inputs::Base
   def run(queue)
     Stud.interval(@interval) do
       @urls.each do |name, url|
-        @logger.debug? && @logger.debug("Checking url ", :name => name , :url => url)
-        begin
-          RestClient::Request.execute(method: :get, url: url, timeout: timeout, accept: 'json'){ |response, request, result, &block|
-            @codec.decode(response) do |event|
-              event["name"] = name
-              event["host"] = @host
-              event["url"] = url
-              event["success"] = true
-              event["responseCode"] = response.code
-
-              case response.code
-              when 200
-                event["success"] = true
-              else
-                event["success"] = false
-              end
-              decorate(event)
-              queue << event
-            end
-          }
-        rescue
-          @logger.error("cannot read URL", :name => name, :url => url)
+        client.async.get(url) do |req|
+          req.on_success {|response| handle_success(name, url, response)}
+          req.on_failure {|exception| handle_failure(name, url, exception)}
         end
-      end #urls.each
-    end #interval loop
-  end #def run
+      end
+      client.execute!
+    end
+  end
 
+  private
+  def handle_success(name, url, response)
+    @codec.decode(response) do |event|
+      event["name"] = name
+      event["host"] = @host
+      event["url"] = url
+      event["success"] = true
+      event["responseCode"] = response.code
+
+      case response.code
+        when 200
+          event["success"] = true
+        else
+          event["success"] = false
+      end
+      decorate(event)
+      queue << event
+    end
+  end
+
+  private
+  def handle_failure(name, url, exception)
+    @logger.error("Cannot read URL! (#{exception}/#{exception.message})", :name => name, :url => url)
+  end
+
+
+
+  public
+  def client_config
+    c = {
+      connect_timeout: @connect_timeout,
+      socket_timeout: @socket_timeout,
+      request_timeout: @request_timeout,
+      follow_redirects: @follow_redirects,
+      automatic_retries: 3,
+      pool_max: @pool_max,
+      pool_max_per_route: @pool_max_per_route
+    }
+
+    if (@trust_store_path)
+      c.merge!(
+        truststore: @trust_store_path
+      )
+
+      if (@trust_store_password)
+        c.merge!(truststore_password: @trust)
+      end
+    end
+
+    c
+  end
+
+  public
+  def client
+    @client ||= Manticore::Client.new(client_config)
+  end
 end
--- logstash-input-http-poller.gemspec
+++ logstash-input-http-poller.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'logstash-input-http-poller'
-  s.version         = '0.0.1'
+  s.version         = '0.0.4'
   s.licenses = ['Apache License (2.0)']
   s.summary = "Placeholder"
   s.description = "This gem is a logstash plugin required to be installed on top of the Logstash core pipeline using $LS_HOME/bin/plugin install gemname. This gem is not a stand-alone program"
@@ -21,6 +21,7 @@ Gem::Specification.new do |s|
   s.add_runtime_dependency 'logstash-codec-plain'
   s.add_runtime_dependency 'logstash-codec-json'
   s.add_runtime_dependency 'stud'
-  s.add_runtime_dependency 'rest-client'
+  s.add_runtime_dependency 'manticore'
+  
   s.add_development_dependency 'logstash-devutils'
 end
