b5a80161302e133bc9e346fdb77029b77c4d2875
Kaise Cheng <kaise.cheng@elastic.co>
2021-07-08 16:18:01 +0200

    remove unused import

--- lib/logstash/inputs/http_poller.rb
+++ lib/logstash/inputs/http_poller.rb
@@ -9,17 +9,14 @@ require "logstash/plugin_mixins/ecs_compatibility_support"
 require 'logstash/plugin_mixins/ecs_compatibility_support/target_check'
 require 'logstash/plugin_mixins/validator_support/field_reference_validation_adapter'
 require 'logstash/plugin_mixins/event_support/event_factory_adapter'
-require 'logstash/plugin_mixins/event_support/from_json_helper'
 
 class LogStash::Inputs::HTTP_Poller < LogStash::Inputs::Base
   include LogStash::PluginMixins::HttpClient
   include LogStash::PluginMixins::ECSCompatibilitySupport(:disabled, :v1, :v8 => :v1)
   include LogStash::PluginMixins::ECSCompatibilitySupport::TargetCheck
+  include LogStash::PluginMixins::EventSupport::EventFactoryAdapter
 
   extend LogStash::PluginMixins::ValidatorSupport::FieldReferenceValidationAdapter
-
-  include LogStash::PluginMixins::EventSupport::EventFactoryAdapter
-  include LogStash::PluginMixins::EventSupport::FromJsonHelper
 
   config_name "http_poller"
 
