486290f03f5a543da674f5fe092d8d65b27e6170
kares <self@kares.org>
2022-02-14 18:56:03 +0100

    sleep for a bit

--- spec/inputs/http_poller_spec.rb
+++ spec/inputs/http_poller_spec.rb
@@ -570,19 +570,22 @@ describe LogStash::Inputs::HTTP_Poller do
       try(5) { expect(plugin_thread).to be_alive }
       # now let's actually stop the plugin
       subject.do_stop
+      sleep 1.5
 
-      require 'jruby'
-      runtime = org.jruby.management.Runtime.new(JRuby.runtime)
-      puts runtime.thread_dump
-      puts '-' * 150
+      if plugin_thread.alive?
+        require 'jruby'
+        runtime = org.jruby.management.Runtime.new(JRuby.runtime)
+        puts runtime.thread_dump
+        puts '-' * 150
+      end
 
       puts "1PLUGIN THREAD alive? : #{plugin_thread.alive?.inspect}"
 
       begin
         try(10) { expect(plugin_thread).to_not be_alive }
       ensure
-        puts "2PLUGIN THREAD alive? : #{plugin_thread.alive?.inspect}"
-        puts runtime.thread_dump
+        # puts "2PLUGIN THREAD alive? : #{plugin_thread.alive?.inspect}"
+        # puts runtime.thread_dump
       end
     end
   end
