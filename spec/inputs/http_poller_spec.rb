f3978267a06165e60bc1ae8395b856aba8503b9f
kares <self@kares.org>
2022-02-14 15:42:03 +0100

    puts plugin_thread.alive?

--- spec/inputs/http_poller_spec.rb
+++ spec/inputs/http_poller_spec.rb
@@ -574,10 +574,12 @@ describe LogStash::Inputs::HTTP_Poller do
       puts runtime.thread_dump
       puts '-' * 150
 
+      puts "1PLUGIN THREAD alive? : #{plugin_thread.alive?.inspect}"
+
       try(10) { expect(plugin_thread).to_not be_alive }
-      if plugin_thread.alive?
-        puts runtime.thread_dump
-      end
+
+      puts "2PLUGIN THREAD alive? : #{plugin_thread.alive?.inspect}"
+      puts runtime.thread_dump
     end
   end
 end
